8c707388fc1ac7f5d731e4a9f2f3ad2159b02dbf
Armagan Ersoz <broccolinisoup@Armagans-MacBook-Pro.local>
2014-01-26 16:44:54 +0200

    initial commit

---
+++ armaganersoz/home/views.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python2.7
+from django.views.generic import TemplateView
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    def get(self, request, *args, **kwargs):
+        context = {
+            'some_dynamic_value': 'This text comes from django view!',
+        }
+        return self.render_to_response(context)
