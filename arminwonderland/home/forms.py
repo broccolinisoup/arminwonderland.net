42813b802e87c74863aa9130f9f8e8b8b4055e7e
arm <armagan.ersoz@gmail.com>
2014-12-07 14:21:41 +0200

    form has been dynamic

--- arminwonderland/home/forms.py
+++ arminwonderland/home/forms.py
@@ -1,8 +1,23 @@
 from django import forms
 from django.forms import ModelForm
 from arminwonderland.home.models import Contacts
+from django.forms.extras import widgets
 
-class ContactForm(ModelForm):
-    class Meta:
-    	model = Contacts
-    	fields = ['name', 'email', 'content']
+class DifferentlySizedTextarea(forms.Textarea):
+  def __init__(self, *args, **kwargs):
+    attrs = kwargs.setdefault('attrs', {})
+    attrs.setdefault('cols', 80)
+    attrs.setdefault('rows', 5)
+    super(DifferentlySizedTextarea, self).__init__(*args, **kwargs)
+
+
+
+class ContactForm(forms.Form):
+
+    name = forms.CharField(label='Name', required=True, max_length=100, widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Name', 'id':'name'}))
+    email= forms.EmailField(label='Email Address', required=True, max_length=100, widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Email Address', 'id':'email'}))
+    content= forms.CharField(label='Message', required=True, max_length=500, widget=DifferentlySizedTextarea(attrs={'class':'form-control','placeholder':'Message', 'id':'message'}))
+
+    #class Meta:
+    	##model = Contacts
+    	#fields = ['name', 'email', 'content']
